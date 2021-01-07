bd9f4de3a700f15286d226c1fb76f3e1da754a3e
Drew Bradley <williamandrewb@gmail.com>
2021-01-07 08:20:58 -0700

    Write methods to add, delete, and filter recipesToCook array.

--- src/user.js
+++ src/user.js
@@ -4,7 +4,7 @@ class User {
     this.name = name;
     this.pantry = pantry;
     this.favoriteRecipes = [];
-
+    this.recipesToCook = [];
   }
 
   addToFavorites(recipe) {
@@ -26,6 +26,25 @@ class User {
 
   findFavorites(strgToSrch) {
     return this.favoriteRecipes.filter(recipe => {
+      return recipe.name.includes(strgToSrch)
+      || recipe.ingredients.find(ingredient => {
+        return ingredient.name.includes(strgToSrch)
+      });
+    });
+  }
+
+  addToRecipesToCook(recipe) {
+    if (!this.recipesToCook.includes(recipe)) {
+      this.recipesToCook.push(recipe)
+    }
+  }
+
+  removeFromRecipesToCook(recipe) {
+    const i = this.recipesToCook.indexOf(recipe);
+    this.recipesToCook.splice(i, 1)
+  }
+  filterRecipesToCook(recipe) {
+    return this.recipesToCook.filter(recipe => {
       return recipe.name.includes(strgToSrch)
       || recipe.ingredients.find(ingredient => {
         return ingredient.name.includes(strgToSrch)

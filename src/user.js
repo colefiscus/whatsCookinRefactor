1fa42fbb6979873bed99787012348d781dcb3819
Drew Bradley <williamandrewb@gmail.com>
2021-01-07 08:24:31 -0700

    Refactor add, delete, and filter to take in an argument of array to be more dynamic and DRY code

--- src/user.js
+++ src/user.js
@@ -7,44 +7,25 @@ class User {
     this.recipesToCook = [];
   }
 
-  addToFavorites(recipe) {
-    if (!this.favoriteRecipes.includes(recipe)) {
-      this.favoriteRecipes.push(recipe)
+  addToRecipeArray(recipe, array) {
+    if (!array.includes(recipe)) {
+      array.push(recipe)
     }
   }
 
-  removeFromFavorites(recipe) {
-    const i = this.favoriteRecipes.indexOf(recipe);
-    this.favoriteRecipes.splice(i, 1)
+  removeFromRecipeArray(recipe, array) {
+    const i = array.indexOf(recipe);
+    array.splice(i, 1)
   }
 
-  filterFavorites(tag) {
-    return this.favoriteRecipes.filter(recipe => {
+  filterRecipeArray(tag, array) {
+    return array.filter(recipe => {
       return recipe.tags.includes(tag);
     });
   }
 
   findFavorites(strgToSrch) {
     return this.favoriteRecipes.filter(recipe => {
-      return recipe.name.includes(strgToSrch)
-      || recipe.ingredients.find(ingredient => {
-        return ingredient.name.includes(strgToSrch)
-      });
-    });
-  }
-
-  addToRecipesToCook(recipe) {
-    if (!this.recipesToCook.includes(recipe)) {
-      this.recipesToCook.push(recipe)
-    }
-  }
-
-  removeFromRecipesToCook(recipe) {
-    const i = this.recipesToCook.indexOf(recipe);
-    this.recipesToCook.splice(i, 1)
-  }
-  filterRecipesToCook(recipe) {
-    return this.recipesToCook.filter(recipe => {
       return recipe.name.includes(strgToSrch)
       || recipe.ingredients.find(ingredient => {
         return ingredient.name.includes(strgToSrch)

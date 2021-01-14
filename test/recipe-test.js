e743424996d32b91574012496255148d0194195c
Drew Bradley <williamandrewb@gmail.com>
2021-01-13 17:18:58 -0700

    Update tests for new data sets.

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -130,7 +130,64 @@ let recipeData = [{
     "hor d'oeuvre"
   ]
 }];
-import ingredientsData from '../src/data/ingredients.js';
+
+let ingredientsData = [
+  {
+    "id": 20081,
+    "name": "wheat flour",
+    "estimatedCostInCents": 142
+  },
+  {
+    "id": 18372,
+    "name": "bicarbonate of soda",
+    "estimatedCostInCents": 582
+  },
+  {
+    "id": 1123,
+    "name": "eggs",
+    "estimatedCostInCents": 472
+  },
+  {
+    "id": 19335,
+    "name": "sucrose",
+    "estimatedCostInCents": 902
+  },
+  {
+    "id": 19206,
+    "name": "instant vanilla pudding",
+    "estimatedCostInCents": 660
+  },
+  {
+    "id": 19334,
+    "name": "brown sugar",
+    "estimatedCostInCents": 559
+  },
+  {
+    "id": 2047,
+    "name": "salt",
+    "estimatedCostInCents": 280
+  },
+  {
+    "id": 1012047,
+    "name": "fine sea salt",
+    "estimatedCostInCents": 528
+  },
+  {
+    "id": 10019903,
+    "name": "semi sweet chips",
+    "estimatedCostInCents": 253
+  },
+  {
+    "id": 1145,
+    "name": "unsalted butter",
+    "estimatedCostInCents": 617
+  },
+  {
+    "id": 2050,
+    "name": "vanilla",
+    "estimatedCostInCents": 926
+  },
+]
 
 let recipe;
 
@@ -149,9 +206,4 @@ describe('Recipe', () => {
       expect(recipe.instructions).to.equal(recipeData[0].instructions);
     })
   })
-
-  it('Should be able to calculate the cost of its ingredients', () => {
-    expect(recipe.calculateCost()).to.equal(17776);
-  });
-
 });
